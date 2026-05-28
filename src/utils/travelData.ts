a0ef0f7afc0a21a952a45b3d76f8d26cae8fb8b1
preetash88 <sahupreetamkumar@gmail.com>
2026-05-28 10:17:23 +0530

    more destinations

--- src/utils/travelData.ts
+++ src/utils/travelData.ts
@@ -1,8 +1,14 @@
 import iceland from "../assets/destinations/iceland1.avif";
 import bali from "../assets/destinations/bali1.avif";
 import kerala from "../assets/destinations/kerala1.avif";
-// import norway from "../assets/destinations/norway.avif";
-// import switzerland from "../assets/destinations/switzerland.avif";
+import norway from "../assets/destinations/norway2.avif";
+import yosemite from "../assets/destinations/yosemite1.avif";
+import antarctica from "../assets/destinations/antarctica1.avif";
+import canada from "../assets/destinations/canadian1.avif";
+import italy from "../assets/destinations/italy1.avif";
+import netherlands from "../assets/destinations/netherlands1.avif";
+import spain from "../assets/destinations/spain1.avif";
+import sydney from "../assets/destinations/sydney1.avif";
 
 export const destinations = [
     {
@@ -25,17 +31,60 @@ export const destinations = [
         image: kerala,
     },
 
-    // {
-    //     id: 3,
-    //     title: "NORWAY",
-    //     location: "Frozen Fjords",
-    //     image: norway,
-    // },
-    //
-    // {
-    //     id: 4,
-    //     title: "SWITZERLAND",
-    //     location: "Alpine Dreams",
-    //     image: switzerland,
-    // },
+    {
+        id: 4,
+        title: "NORWAY",
+        location: "Norway Fjords",
+        image: norway,
+    },
+    {
+        id: 5,
+        title: "YOSEMITE",
+        location: "American National Parks",
+        image: yosemite,
+    },
+
+    {
+        id: 6,
+        title: "ANTARCTICA",
+        location: "Icy Antarctica",
+        image: antarctica,
+    },
+
+    {
+        id: 7,
+        title: "CANADA",
+        location: "Canadian Rockies",
+        image: canada,
+    },
+
+    {
+        id: 8,
+        title: "ITALY",
+        location: "Italian Alps",
+        image: italy,
+    },
+
+    {
+        id: 9,
+        title: "NETHERLANDS",
+        location: "Dutch Windmills",
+        image: netherlands,
+    },
+
+    {
+        id: 10,
+        title: "SPAIN",
+        location: "Ambient Barcelona",
+        image: spain,
+    },
+
+    {
+        id: 11,
+        title: "SYDNEY",
+        location: "Sydney Opera House",
+        image: sydney,
+    },
+
+
 ];
